5394790f38df6cc2d7c50b83d97a493a92f74f28
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2022-06-07 00:12:54 +0530

    Time: 17 ms (18.49%), Space: 12.1 MB (18.55%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> ans;
+    
+    void solve(string ip, string op){
+        if(ip.length() == 0)
+        {
+            ans.push_back(op);
+            return;
+        }
+        
+        
+        if(isalpha(ip[0]))
+        {
+            string op1 = op;
+            string op2 = op;
+            op1.push_back( tolower(ip[0]) );
+            op2.push_back( toupper(ip[0]) ); 
+            
+            ip.erase( ip.begin()+0 );
+            
+            solve(ip, op1);
+            solve(ip, op2);
+        }
+        
+        else if(isdigit(ip[0]))
+        {
+            
+            op.push_back(ip[0]);
+            ip.erase( ip.begin()+0 );
+            solve(ip, op);
+        }
+        
+        
+    }
+    
+    vector<string> letterCasePermutation(string ip) {
+        
+        solve(ip, "");
+        return ans;
+    }
+};
