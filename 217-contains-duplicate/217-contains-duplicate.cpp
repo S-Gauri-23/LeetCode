dc626e4597125d9124d6970e5ae85ef69ae96a3f
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2022-08-20 20:21:37 +0530

    Time: 151 ms (50.62%), Space: 46.7 MB (80.14%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) 
-    {
+    bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        
         int N = nums.size();
-        
-        for(int i =0 ; i < N-1 ; i++){
+       
+        for(int i=0; i<N-1; i++){
             if(nums[i] == nums[i+1])
                 return true;
         }
         return false;
-  }
+    }
 };
