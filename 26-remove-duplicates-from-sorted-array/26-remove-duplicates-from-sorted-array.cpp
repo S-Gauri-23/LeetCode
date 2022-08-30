85c1c3441df310e217c6ebb948703d93da9538d7
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2022-08-30 23:13:54 +0530

    Time: 12 ms (85.73%), Space: 18.5 MB (36.79%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,6 +4,8 @@ public:
         int i=0;
         int size = nums.size();
         
+        if(size == 0) return 0;
+        
         for(int j=i+1; j<size ; j++){
             if(nums[i] != nums[j]){
                 i++;
